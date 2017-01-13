4dce0538b605d52bed192da553d4c22cd9479e4c
2f17 <2f17@Liceospalla.local>
2017-01-13 10:46:35 +0100

    inizio

---
+++ EsercizioClasse_Mortara/EsercizioClasse_Mortara/persona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioClasse_Mortara
+{
+    class Persona
+    {
+        private string _name;
+        public string name { get; set; }
+
+        private string _surname;
+        public string surname { get; set;} 
+
+        private int _age;
+        public int age
+        {
+            set
+            {
+                if (_age <= 0) _age = 1;
+            }
+            get
+            {
+                return _age;
+            }
+
+        private string _fCode;
+        public string fCode
+        {
+            set
+            {
+                _fCode += "NOME: " + name;
+                _fCode += "COGNOME: " + surname;
+                _fCode += "ANNI: " + age;
+            }
+            
+        }
+
+
+
+        public void Persona()
+    }
+}
